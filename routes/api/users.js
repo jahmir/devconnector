19dc4f978ef0f2c69e23b29b82af4031100cff6f
jahmir <altairjohnfbacc@gmail.com>
2020-12-18 14:20:14 +0800

    updates on routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const User = require('../../models/User');
 
 const Users = require('../../models/User')
 
@@ -11,7 +14,7 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
-], (req,res) => {
+], async (req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() })
@@ -19,15 +22,41 @@ router.post('/', [
 
     const { name, email, password } = req.body;
 
-    //see if user exists
+    try {
+        let user = await User.findOne({ email })
+        //see if user exists
+        if(user){
+            return res.status(400).json({ errors: [{msg: 'User already exists'}]})
+        }
 
-    //Get users gravatar
+        //Get users gravatar
+        const avatar = gravatar.url({
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
 
-    //Encrypt password
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
 
-    //Return jsonwebtoken
+        //Encrypt password
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
 
-    res.send()
+        //save user to db
+        await user.save()
+
+        //Return jsonwebtoken
+        res.send('User registered')
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error')
+    }
 })
 
 module.exports = router;
