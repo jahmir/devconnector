5e69e3835ddacfcd42d88907f02290ea67bff69f
jahmir <altairjohnfbacc@gmail.com>
2020-12-22 14:26:22 +0800

    updates added redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>
+<Provider store={store}>
+  <Router>
+    <Fragment>
+      <Navbar></Navbar>
+      <Route exact path='/' component={Landing}></Route>
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+  </Provider>
+  
 
 export default App;
